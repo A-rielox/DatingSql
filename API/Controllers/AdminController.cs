861fe3c419018bf2f8f7d72c6bcdad20db1fd19a
A-rielox <arielox.ag@gmail.com>
2023-10-04 21:32:08 -0600

    XD

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -24,20 +24,6 @@ public class AdminController : BaseApiController
     [HttpGet("users-with-roles")]
     public async Task<ActionResult<List<FromDbUserForRoles>>> GetUsersWithRoles()
     {
-        // de .Users tengo una related table ( userRoles, con una related table
-        // q son los roles ) ( supongo q una navigation prop ) q va a los roles
-        //var users = await _userManager.Users
-        //            .OrderBy(u => u.UserName)
-        //            .Select(u => new
-        //            {
-        //                u.Id,
-        //                Username = u.UserName,
-        //                Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
-        //            }).ToListAsync();
-
-
-        //return Ok(users);
-
         List<FromDbUserForRoles> users;
         List<FromDbRoleForRoles> roles;
 
@@ -62,36 +48,7 @@ public class AdminController : BaseApiController
     [HttpPost("edit-roles/{username}")] // deberia ser PUT xq se esta actualizando
     public async Task<ActionResult<List<string>>> EditRoles(string username, [FromQuery] string roles)
     {
-        /**/
         if (string.IsNullOrEmpty(username)) return BadRequest("You must select at least one role.");
-
-        //var selectedRoles = roles.Split(",").ToList();
-
-        // arreglar estoy solo mandando la lista de nuevos roles
-
-        //List<FromDbUserForRoles> usersList;
-        //List<FromDbRoleForRoles> rolesList;
-
-        //using (var lists = await db.QueryMultipleAsync("sp_editRoles",
-        //                            // @userName NVARCHAR(50),
-        //                            //@rolesList NVARCHAR(500)
-        //                            new { userName = username, rolesList = roles },
-        //                            commandType: CommandType.StoredProcedure))
-        //{
-        //    usersList = lists.Read<FromDbUserForRoles>().ToList();
-        //    rolesList = lists.Read<FromDbRoleForRoles>().ToList();
-        //}
-
-        //usersList.ForEach(u =>
-        //{
-        //    u.Roles = rolesList.Where(r => r.UserId == u.Id).Select(r => r.name).ToList();
-        //});
-
-
-        ////// retorno la lista actualizado de los roles q se tiene
-
-
-        //return Ok(usersList);
 
         var newRoles = await db.QueryAsync<string>("sp_editRoles",
                                     new { userName = username, rolesList = roles },
